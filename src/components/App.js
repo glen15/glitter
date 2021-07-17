c3831a45e6747d950f180bbe132ce743c58f64c6
glen <glen910115@gmail.com>
2021-07-17 14:13:49 +0900

    using firebase auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import AppRouter from "./Router";
-import fbase from "../fbase";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); //currentUser 는 username또는 null 반환
   // props로 로그인상태를 <AppRouter>로 보내줘야한다
   return (
   <>
