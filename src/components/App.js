1d05271a461358da6a5aba25bd2740aa0a822c40
glen <glen910115@gmail.com>
2021-07-17 15:59:21 +0900

    log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AppRouter from "components/Router";
 import { authService } from "fbase";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser); //currentUser 는 username또는 null 반환
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false); //currentUser 는 username또는 null 반환
   // props로 로그인상태를 <AppRouter>로 보내줘야한다
+
+  useEffect(()=> {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
   <>
-    <AppRouter isLoggedIn={ isLoggedIn } />
+    {init ? <AppRouter isLoggedIn={ isLoggedIn } />: "Initializing..."}
     <footer>@copy; {new Date().getFullYear()} Glitter By glen</footer>
   </>
   )
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,6 +5,7 @@ import React, { useState } from "react";
      const [email, setEmail] = useState("");
      const [password, setPassword] = useState("");
      const [newAccount, setNewAccount] = useState(true);
+     const [error, setError] = useState("");
      const onChange = (event) => {
          const {target: {name, value}} = event;
          if (name === "email"){
@@ -29,17 +30,20 @@ import React, { useState } from "react";
             }
             console.log(data)
          } catch(error) {
-            console.log(error)
+            setError(error.message)
          }
-     }
+     };
+     const toogleAccount = () => setNewAccount(prev => !prev);
 
      return (
         <div>
             <form onSubmit={onSubmit}>
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-                <input type="submit" value={newAccount ? "Create Account" : "Log in"} />
+                <input type="submit" value={newAccount ? "Create Account" : "Sign in"} />
+                {error}
             </form>
+            <span onClick={toogleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
             <div> 
                 <button>Continue with Google</button>
                 <button>Continue with GitHub</button>
