cd2d1330a0b44271eb48649b6f156c06065b7189
glen <glen910115@gmail.com>
2021-07-17 15:15:08 +0900

    create new account

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,19 +1,36 @@
+import { authService } from "fbase";
 import React, { useState } from "react";
 
  const Auth = () => {
      const [email, setEmail] = useState("");
      const [password, setPassword] = useState("");
+     const [newAccount, setNewAccount] = useState(true);
      const onChange = (event) => {
          const {target: {name, value}} = event;
          if (name === "email"){
              setEmail(value)
-         } else if (name === "passwo rd"){
+         } else if (name === "password"){
              setPassword(value)
          }
      }
-     const onSubmit = (event) => {
+     const onSubmit = async(event) => {
          event.preventDefault(); // 이거 안하면, 로그인버튼 누를때마다 새로고침되고, email password에 적용한 리액트기능이 다 사라진다
          //preventDefault() : 기본행위가 실행되는것 막기
+         try {
+             let data
+            if(newAccount){
+                data = await authService.createUserWithEmailAndPassword(
+                    email, password
+                );
+            } else {
+                data = await authService.signInWithEmailAndPassword(
+                    email, password
+                );
+            }
+            console.log(data)
+         } catch(error) {
+            console.log(error)
+         }
      }
 
      return (
@@ -21,7 +38,7 @@ import React, { useState } from "react";
             <form onSubmit={onSubmit}>
                 <input name="email" type="email" placeholder="Email" required value={email} onChange={onChange}/>
                 <input name="password" type="password" placeholder="Password" required value={password} onChange={onChange}/>
-                <input type="submit" value="Log In" />
+                <input type="submit" value={newAccount ? "Create Account" : "Log in"} />
             </form>
             <div> 
                 <button>Continue with Google</button>
