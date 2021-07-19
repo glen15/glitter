b96e685df41a07dfe9f1ac11550bee0f76b95ce3
glen <glen910115@gmail.com>
2021-07-19 09:55:32 +0900

    form and database setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,4 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Home = () => <span>Home</span>;
+const Home = () => {
+    const [gleet, setGleet] = useState("");
+    const onSubmit = (event) => {
+        event.preventDefault(); //submit 클릭시 새로고침되는거 막으려고
+    };
+    const onChange = (event) => {
+        const { 
+            target: {value},
+        } = event;
+        // event 안에있는 target 안에있는 value를 내놔라는것
+        setGleet(value);
+    };
+    return (
+    <div>
+        <form onSubmit={onSubmit}>
+            <input value={gleet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+            <input type="submit" value="Gleet" />
+        </form>
+    </div>
+    )
+};
 export default Home; 
