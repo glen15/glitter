f20d0883b7c11c5c239d0c7a14376de2a10d2bc8
glen <glen910115@gmail.com>
2021-07-21 09:48:23 +0900

    update and delete part 2

--- src/components/Gleet.js
+++ src/components/Gleet.js
@@ -1,15 +1,60 @@
-import React from "react";
+import { dbService } from "fbase";
+import React, {useState} from "react";
 
-const Gleet = ({gleetObj, isOwner}) => ( //이거 왜 {}쓰면 안되는거지
-    <div>
-        <h4>{gleetObj.text}</h4>
-        {isOwner && 
-        <>
-        <button>Delete Gleet</button>
-        <button>Edit Gleet</button>
-        </>
+const Gleet = ({gleetObj, isOwner}) => {
+    const [editing, setEditing] = useState(false);
+    const [newGleet, setNewGleet] = useState(gleetObj.text);
+    
+    const onDeleteClick = async () => {
+        const ok = window.confirm("Are you sure you want to delete this gleet?");
+        if (ok) {
+            await dbService.doc(`gleets/${gleetObj.id}`).delete();
+            //doc 의 id를 알고있기 때문에 가능했지. 이건 home에서 보내준 gleetObj덕분이고
+            //gleets collection에서 id로 document를 찾아온거 / firebase callection 보면 확인가능
         }
-    </div>
-)
+    }
+
+    const toggleEditing = () => setEditing((prev) => !prev);
+    //전에 값을 받아서 반대로 바꿔서 저장
+    //편집모드로 들어가게할 때 이렇게 쓰면 좋겠다. useState로 true false 바꾸면서
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        await dbService.doc(`gleets/${gleetObj.id}`).update({
+            text:newGleet
+        });
+        setEditing(false)
+    };
+    const onChange = (event) => {
+        const {target:{value},} = event;
+        setNewGleet(value);
+        //이거해야 새로 작성하는 수정하기 글이 보이지
+    }
+
+    return (
+        <div>
+            {
+                editing? (
+                    <>
+                        <form onSubmit={onSubmit}>
+                            <input onChange={onChange} type="text" placeholder="Edit your gleet" value={newGleet} required/>
+                            <input type="submit" value="Update" />
+                        </form>
+                        <button onClick={toggleEditing}>Cancel</button>
+                    </>
+                 ) : (
+                <>
+                    <h4>{gleetObj.text}</h4>
+                    {isOwner && 
+                        <>
+                            <button onClick={onDeleteClick}>Delete Gleet</button>
+                            <button onClick={toggleEditing}>Edit Gleet</button>
+                        </>
+                    }
+                </>
+                 )
+            }
+        </div>
+    )
+}
 
 export default Gleet;
