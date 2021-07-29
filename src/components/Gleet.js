d6ecf0ed6a15e7d04117aff7abd1f0b1abd22630
glen <glen910115@gmail.com>
2021-07-29 20:40:05 +0900

    Preview Images part One

--- src/components/Gleet.js
+++ src/components/Gleet.js
@@ -2,13 +2,13 @@ import { dbService } from "fbase";
 import React, {useState} from "react";
 
 const Gleet = ({gleetObj, isOwner}) => {
-    const [editing, setEditing] = useState(false);
-    const [newGleet, setNewGleet] = useState(gleetObj.text);
-    
-    const onDeleteClick = async () => {
+    const  [editing, setEditing] = useState(false); //edit mode 변경을 위해서
+    const  [newGleet, setNewGleet] = useState(gleetObj.text); //edit input에 넣는걸 onChange반영하기 위해서
+     
+    const  onDeleteClick = async () => {
         const ok = window.confirm("Are you sure you want to delete this gleet?");
         if (ok) {
-            await dbService.doc(`gleets/${gleetObj.id}`).delete();
+            await dbService.doc(`gleets/${gleetObj.id}`).delete(); //firebase
             //doc 의 id를 알고있기 때문에 가능했지. 이건 home에서 보내준 gleetObj덕분이고
             //gleets collection에서 id로 document를 찾아온거 / firebase callection 보면 확인가능
         }
@@ -20,7 +20,7 @@ const Gleet = ({gleetObj, isOwner}) => {
     const onSubmit = async (event) => {
         event.preventDefault();
         await dbService.doc(`gleets/${gleetObj.id}`).update({
-            text:newGleet
+            text: newGleet
         });
         setEditing(false)
     };
@@ -32,15 +32,18 @@ const Gleet = ({gleetObj, isOwner}) => {
 
     return (
         <div>
-            {
-                editing? (
-                    <>
-                        <form onSubmit={onSubmit}>
-                            <input onChange={onChange} type="text" placeholder="Edit your gleet" value={newGleet} required/>
-                            <input type="submit" value="Update" />
-                        </form>
-                        <button onClick={toggleEditing}>Cancel</button>
-                    </>
+            {editing? (
+                <>
+                    {isOwner && (
+                        <>
+                            <form onSubmit={onSubmit}>
+                                <input onChange={onChange} type="text" placeholder="Edit your gleet" value={newGleet} required/>
+                                <input type="submit" value="Update" />
+                            </form>
+                            <button onClick={toggleEditing}>Cancel</button>
+                        </>
+                    )}
+                </>
                  ) : (
                 <>
                     <h4>{gleetObj.text}</h4>
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,6 +7,7 @@ const Home = ({ userObj }) => {
     const [gleets, setGleets] = useState([]);
     useEffect(() => {
         dbService.collection("gleets").onSnapshot(snapshot => { //.collection("gleets").orderBy("createdAt","desc").onSnapshot(~~)
+        // snapshot : 데이터베이스에 무슨일이 있을 때 알림을 받음
             const gleetArray = snapshot.docs.map(doc => ({
                 id: doc.id,
                 ...doc.data(),
@@ -30,18 +31,29 @@ const Home = ({ userObj }) => {
         // event 안에있는 target 안에있는 value를 내놔라는것
         setGleet(value);
     };
+    const onFileChnage = (event) => {
+        // console.log(event.target.files); // input이 가진 타겟에서 파일까지 접근
+        const {target:{files},} = event //이벤트안으로 가서 target에서 files가져와라 es6의 기능
+        const theFile = files[0]; // 파일만들고
+        const reader = new FileReader(); //파일리더기 만들고
+        reader.onloadend = (finishedEvent) => {
+            console.log(finishedEvent);
+        }
+        reader.readAsDataURL(theFile); //파일리더기로 파일 읽고
+    };
     return (
     <div>
         <form onSubmit={onSubmit}>
             <input value={gleet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+            <input type="file" accept="image/*" onChange={onFileChnage}/>
             <input type="submit" value="Gleet" />
         </form>
         <div>
             {gleets.map(gleet => (
                 <Gleet
                 key={gleet.id} 
-                gleetObj={gleet}
-                isOwner={gleet.creatorId === userObj.uid}
+                gleetObj={gleet} //모든데이터
+                isOwner={gleet.creatorId === userObj.uid} //userObj는 Router에서 Home으로 보낸 prop
                 />
             ))}
         </div>
