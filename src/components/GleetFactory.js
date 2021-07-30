de88bb2301c9a45108e97009704874d95bf691f0
glen <glen910115@gmail.com>
2021-07-30 12:46:24 +0900

    Cleaning JS Code

---
+++ src/components/GleetFactory.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { dbService, sotrageService } from "fbase";
+import {v4 as uuidv4} from "uuid";
+
+const GleetFactory = ({userObj}) => {
+    const [gleet, setGleet] = useState("");
+    const [attachment, setAttachment] = useState("");
+    const onSubmit = async (event) => { //promise를 리턴하기 때문에 async 넣는다
+        event.preventDefault(); //submit 클릭시 새로고침되는거 막으려고
+        let attachmentUrl = "";
+        if (attachment !== ""){
+            const attachmentRef = sotrageService
+                .ref()
+                .child(`${userObj.uid}/${uuidv4()}`); // 파일에 대한 레퍼런스 refFromURL 메소드로 storage에서 찾을 수도 있다
+            const response = await attachmentRef.putString(attachment, "data_url"); //readAsDataURL 했던게 data_url
+            attachmentUrl = await response.ref.getDownloadURL();
+        }
+        const gleetObj = {
+            text: gleet, // 이 gleet는 위에 setState의 gleet값
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+            attachmentUrl,
+        };
+        await dbService.collection("gleets").add(gleetObj);
+        setGleet(""); //submit 했으니 다시 비워주는 것
+        setAttachment("");
+    };
+    const onChange = (event) => {
+        const { 
+            target: {value},
+        } = event;
+        // event 안에있는 target 안에있는 value를 내놔라는것
+        setGleet(value);
+    };
+    const onFileChange = (event) => {
+        // console.log(event.target.files); // input이 가진 타겟에서 파일까지 접근
+        const {target:{ files },} = event; //이벤트안으로 가서 target에서 files가져와라 es6의 기능
+        const theFile = files[0]; // 파일만들고
+        const reader = new FileReader(); //파일리더기 만들고
+        reader.onloadend = (finishedEvent) => {
+            const {currentTarget:{result},} = finishedEvent;
+            setAttachment(result);
+        };
+        reader.readAsDataURL(theFile); //파일리더기로 파일 읽고
+    };
+    const onClearAttachment = () => setAttachment(""); //사진 미리보기 없애기
+    return (<form onSubmit={onSubmit}>
+        <input value={gleet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange}/>
+        <input type="submit" value="Gleet" />
+        {attachment && (
+            <div>
+                <img src={attachment} width="50px" height="50px" />
+                <button onClick={onClearAttachment}>Clear</button>
+            </div>
+        )}
+</form>
+    )}
+
+export default GleetFactory;
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,39 +1,8 @@
+import React from "react";
 import { authService, firebaseInstance } from "fbase";
-import React, { useState } from "react";
+import AuthForm from "components/AuthForm"
 
  const Auth = () => {
-     const [email, setEmail] = useState("");
-     const [password, setPassword] = useState("");
-     const [newAccount, setNewAccount] = useState(true);
-     const [error, setError] = useState("");
-     const onChange = (event) => {
-         const {target: {name, value}} = event;
-         if (name === "email"){
-             setEmail(value)
-         } else if (name === "password"){
-             setPassword(value)
-         }
-     }
-     const onSubmit = async(event) => {
-         event.preventDefault(); // 이거 안하면, 로그인버튼 누를때마다 새로고침되고, email password에 적용한 리액트기능이 다 사라진다
-         //preventDefault() : 기본행위가 실행되는것 막기
-         try {
-             let data
-            if(newAccount){
-                data = await authService.createUserWithEmailAndPassword(
-                    email, password
-                );
-            } else {
-                data = await authService.signInWithEmailAndPassword(
-                    email, password
-                );
-            }
-            console.log(data)
-         } catch(error) {
-            setError(error.message)
-         }
-     };
-     const toogleAccount = () => setNewAccount(prev => !prev);
      const onSocialClick = async (event) => {
         const {target:{name}} = event;
         let provider; //social login 을 위한 장치 반드시 필요
@@ -45,27 +14,9 @@ import React, { useState } from "react";
         const data = await authService.signInWithPopup(provider);
         console.log(data);
      };
-
      return (
         <div>
-            <form onSubmit={onSubmit}>
-                <input name="email" 
-                type="email" 
-                placeholder="Email" 
-                required 
-                value={email} 
-                onChange={onChange}/>
-                <input name="password" 
-                type="password" 
-                placeholder="Password" 
-                required 
-                value={password} 
-                onChange={onChange}/>
-                <input type="submit" 
-                value={newAccount ? "Create Account" : "Sign in"} />
-                {error}
-            </form>
-            <span onClick={toogleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+            <AuthForm />
             <div> 
                 <button name="google" onClick={onSocialClick}>
                     Continue with Google
